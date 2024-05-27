768e60fb19bc962c6ea87b6b3ea6861af1138415
Isaac Blanco <63171746+isaacblanco@users.noreply.github.com>
2024-05-27 21:20:36 +0200

    He decidido borrar los test de las pipes
    Por que tenga menos contaminación de archivos

--- PEC6_Ej_Prac/src/app/shared/article-image.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ArticleImagePipe } from './article-image.pipe';
-
-describe('ArticleImagePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ArticleImagePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- PEC6_Ej_Prac/src/app/shared/formato-moneda.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FormatoMonedaPipe } from "./shared/formato-moneda.pipe";
-
-describe("FormatoMonedaPipe", () => {
-  it("create an instance", () => {
-    const pipe = new FormatoMonedaPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
